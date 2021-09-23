67e9837b07d2187f841c134f928a1b06b2b2327a
jirafek <nikolaevserafim15@gmail.com>
2021-09-23 23:29:57 +0300

    feat: cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  function countCats(backyard) {
+    let arr = backyard.toString().split(",");
+    let sum = 0;
+    for(let i=0;i<arr.length;i++) {
+      sum += arr[i] === "^^" ? 1 : 0 
+    }
+    return sum
+  }
 }
